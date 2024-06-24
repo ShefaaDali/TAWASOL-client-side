0e4bb144108486df5de517412ef884cb9f9c65bf
ShefaaDali <shefaadali@gmail.com>
2024-06-24 18:59:28 +0300

    update app.js | add navbar and login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css';
 import Landing from './components/Landing';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
     <>
+    <Navbar/>
     <Routes>
       <Route exact path="/" element={<Landing/>}/>
+      <Route exact path="/login" element={<Landing/>}/>
+
     </Routes>
     </>
     </BrowserRouter>
